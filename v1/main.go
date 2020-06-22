63cf09d81661b7cc09a9bc0d51c8a44f9a91fdcd
congliqiang <2785347264@qq.com>
2020-06-22 16:59:55 +0800

    v1本地对象存储

---
+++ v1/main.go
@@ -0,0 +1,12 @@
+package v1
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/objects/", objects.Handler)
+	log.Fatal(http.ListenAndServe(os.Getenv("LISTEN_ADDRESS"), nil))
+}
